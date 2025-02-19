8169ead04f6ea29ee77742b480fb4d2cebc076d6
Nayan Jain <nayanjain632@gmail.com>
2025-02-19 18:01:20 +0000

    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 #app is the object of the class Flask __name__ is the name of the file
@@ -21,7 +21,7 @@ JOBS=[
     'title':'Frontend Engineer',
     'location':'Remote',
     'salary':'Rs. 12,00,000'   
-  }
+  },
   {
     'id':4,
     'title':'Backend Engineer',
@@ -34,8 +34,11 @@ JOBS=[
 @app.route("/")
 #this is a decorator - when this url is requested what shld be returned - "/"empty route
 def hello_world():
-  return render_template("home.html", jobs=JOBS)
+  return render_template("home.html", jobs=JOBS, company_name="Jovian")
 
+@app.route("/api/jobs")#api route of the jobs
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)  #host is set to 0 that to run locally
