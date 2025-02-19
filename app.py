d90e0ad1a30008ec4577843591dc23f491b4acda
Nayan Jain <nayanjain632@gmail.com>
2025-02-19 17:35:13 +0000

    demo commit

--- app.py
+++ app.py
@@ -3,11 +3,38 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 #app is the object of the class Flask __name__ is the name of the file
 
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengaluru, India',
+    'salary':'Rs. 10,00,000'   
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi, India',
+    'salary':'Rs. 15,00,000'   
+  },
+  {
+    'id':3,
+    'title':'Frontend Engineer',
+    'location':'Remote',
+    'salary':'Rs. 12,00,000'   
+  }
+  {
+    'id':4,
+    'title':'Backend Engineer',
+    'location':'San Francisco, USA',
+    'salary':'$120,000'
+  }
+]
+
 
 @app.route("/")
 #this is a decorator - when this url is requested what shld be returned - "/"empty route
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
